b76b06d3497d8cd2cedec99f218f9aa37d922fc2
yChoon <36335272+ychoon94@users.noreply.github.com>
2021-10-18 17:15:07 +0800

    Completion of report chap4
    Minor code cleanup
    Report mark completion for chap4

--- 2D Pixel RPG project/Assets/Scripts/QuestObjectActivator.cs
+++ 2D Pixel RPG project/Assets/Scripts/QuestObjectActivator.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class QuestObjectActivator : MonoBehaviour
 {
     public GameObject objectToActivate;
-
     public string questToCheck;
-
     public bool activeIfComplete;
-
     private bool initialCheckDone;
 
     // Start is called before the first frame update
